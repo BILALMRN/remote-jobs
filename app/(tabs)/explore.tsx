ec12dc246a181f8df7f1006def65dab45c4fc47e
BILALMRN <bilalmrn.mail@gmail.com>
2024-10-14 18:37:29 +0100

    dev: Fixed

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -13,33 +13,28 @@ export default function TabTwoScreen() {
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Overview</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
+      <ThemedText>Browse through a wide range of job listings tailored to your skills and interests. 
+        From technology and marketing to customer support and creative roles,
+         we&apos;ve got something for everyone.</ThemedText>
 
-
-      <Collapsible title="Privacy Policy">
-        <ThemedText>
-          Welcome to [App Name]! Your privacy is important to us.
-          This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our app.
+      <Collapsible title="Explore">
+      <ThemedText type="defaultSemiBold">
+          <ExternalLink href="#">
+            <ThemedText type="link">Website</ThemedText>
+          </ExternalLink>
         </ThemedText>
-        <ExternalLink href="#">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-
-      <Collapsible title="Terms Of Use">
-        <ThemedText>
-          By accessing or using [App Name], you agree to comply with and be bound by these Terms of Use.
-          If you do not agree, please do not use the app.
+        <ThemedText type="defaultSemiBold">
+          <ExternalLink href="#">
+            <ThemedText type="link">Privacy Policy</ThemedText>
+          </ExternalLink>
         </ThemedText>
-        <ExternalLink href="#">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-
-      <Collapsible title="//\\">
-
+        <ThemedText type="defaultSemiBold">
+          <ExternalLink href="#">
+            <ThemedText type="link">Terms Of Use</ThemedText>
+          </ExternalLink>
+        </ThemedText>
         <ThemedText type="defaultSemiBold">
           <ExternalLink href="#">
             <ThemedText type="link">Github Repo</ThemedText>
